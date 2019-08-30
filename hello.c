3264512e7e3431dd1baec859ebcd314d91a65a7c
Justin <justinkerr@huskers.unl.edu>
2019-08-29 23:28:43 -0500

    Updated with APPOLLO7's name/major

--- hello.c
+++ hello.c
@@ -6,6 +6,9 @@ int main(int argc, char **argy) {
     printf("Justin Kerr\n");
     printf("Computer Engineering\n");
     printf("Physics\n");
-    
+
+    printf("Apollinaire Amomba\n");
+    printf("computer science\n");
+    printf("maths\n");
     return (0);
 }
